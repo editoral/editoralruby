d0edf26c138b2fc88e2bba7583e01ba77300dec6
editoral <marc.porto@hotmail.com>
2012-12-15 13:31:57 +0100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Editoral::Application.config.secret_token = '937ba56c5ba304c3f4119689146d84ea820eaa612fe3537ff4dcca3f369ed3eefaf86c24bba0cbbe1b032aac5e369a8c9e07af9d24c4d0e0bff2a00b2a88ff05'
